0e3856b54f054a50111ba8ff0040327ce696b922
DAB0mB <emanor6@gmail.com>
2016-04-20 23:54:00 +0300

    Step 1.2: Add webpack task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,30 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var webpack = require('webpack');
+
+var webpackConfig = require('./webpack.config');
 
 var paths = {
+  webpack: ['./src/**/*.js', '!./www/lib/**/*'],
   sass: ['./scss/**/*.scss']
 };
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['webpack', 'sass']);
+
+gulp.task('webpack', function(done) {
+  webpack(webpackConfig, function(err, stats) {
+    if (err) {
+      throw new gutil.PluginError('webpack', err);
+    }
+
+    gutil.log('[webpack]', stats.toString({
+      colors: true
+    }));
+
+    done();
+  });
+});
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
@@ -27,6 +45,7 @@ gulp.task('sass', function(done) {
 });
 
 gulp.task('watch', function() {
+  gulp.watch(paths.webpack, ['webpack']);
   gulp.watch(paths.sass, ['sass']);
 });
 
