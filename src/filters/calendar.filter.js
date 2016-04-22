318e9e57c7aaedf183a66d876150933b8a1580f9
DAB0mB <emanor6@gmail.com>
2016-04-22 16:47:16 +0300

    Step 2.13: Add calendar filter

---
+++ src/filters/calendar.filter.js
@@ -0,0 +1,17 @@
+import moment from 'moment';
+import { Filter } from '../entities';
+
+export default class CalendarFilter extends Filter {
+  static $name = 'calendar'
+
+  filter(time) {
+    if (!time) return;
+
+    return moment(time).calendar(null, {
+      lastDay : '[Yesterday]',
+      sameDay : 'LT',
+      lastWeek : 'dddd',
+      sameElse : 'DD/MM/YY'
+    });
+  }
+}
