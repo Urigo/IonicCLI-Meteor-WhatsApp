bd8e6d12afde067ac88c6ce809f8efe6c13210a3
DAB0mB <emanor6@gmail.com>
2016-04-23 12:46:43 +0300

    Step 3.6: Update watch paths in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var webpack = require('webpack');
 var webpackConfig = require('./webpack.config');
 
 var paths = {
-  webpack: ['./src/**/*.js', '!./www/lib/**/*'],
+  webpack: ['./src/**/*.js', '!./www/lib/**/*', './api/server/**/*.js'],
   sass: ['./scss/**/*.scss']
 };
 
