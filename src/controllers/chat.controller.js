02941ec80e6ce7c83c618732e62aab8813c680c5
DAB0mB <emanor6@gmail.com>
2016-04-25 00:23:59 +0300

    Step 4.2: Add chat controller

---
+++ src/controllers/chat.controller.js
@@ -0,0 +1,20 @@
+import { Chats } from 'api/collections';
+import { Controller } from 'angular-ecmascript/module-helpers';
+
+export default class ChatCtrl extends Controller {
+  static $inject = ['$stateParams']
+
+  constructor() {
+    super(...arguments);
+
+    this.chatId = this.$stateParams.chatId;
+
+    this.helpers({
+      data() {
+        return Chats.findOne(this.chatId);
+      }
+    });
+  }
+}
+
+ChatCtrl.$name = 'ChatCtrl';
