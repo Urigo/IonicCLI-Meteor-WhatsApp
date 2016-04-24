1be041c71bb0b65abd52a68d8290f00aa03b2065
DAB0mB <emanor6@gmail.com>
2016-04-25 00:18:45 +0300

    Step 4.4: Add chat route state

--- src/routes.js
+++ src/routes.js
@@ -18,6 +18,15 @@ export default class RoutesConfig extends Config {
             controller: 'ChatsCtrl as chats'
           }
         }
+      })
+      .state('tab.chat', {
+        url: '/chats/:chatId',
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/chat.html',
+            controller: 'ChatCtrl as chat'
+          }
+        }
       });
 
     this.$urlRouterProvider.otherwise('tab/chats');
