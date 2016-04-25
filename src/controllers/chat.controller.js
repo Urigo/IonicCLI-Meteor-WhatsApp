13ab821fd7cb87bc29ff015d125988f2687353c2
DAB0mB <emanor6@gmail.com>
2016-04-26 00:10:24 +0300

    Step 4.20: Implement input events in chat controller

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -1,13 +1,16 @@
+import Ionic from 'ionic';
+import Keyboard from 'cordova/keyboard';
 import { Chats, Messages } from 'api/collections';
 import { Controller } from '../entities';
 
 export default class ChatCtrl extends Controller {
-  static $inject = ['$stateParams']
+  static $inject = ['$stateParams', '$timeout', '$ionicScrollDelegate']
 
   constructor() {
     super(...arguments);
 
     this.chatId = this.$stateParams.chatId;
+    this.isIOS = Ionic.Platform.isWebView() && Ionic.Platform.isIOS();
 
     this.helpers({
       messages() {
@@ -17,5 +20,33 @@ export default class ChatCtrl extends Controller {
         return Chats.findOne(this.chatId);
       }
     });
+  }
+
+  inputUp () {
+    if (this.isIOS) {
+      this.keyboardHeight = 216;
+    }
+
+    this.scrollBottom(true);
+  }
+
+  inputDown () {
+    if (this.isIOS) {
+      this.keyboardHeight = 0;
+    }
+
+    this.$ionicScrollDelegate.$getByHandle('chatScroll').resize();
+  }
+
+  closeKeyboard() {
+    if (Keyboard) {
+      Keyboard.close();
+    }
+  }
+
+  scrollBottom(animate) {
+    this.$timeout(() => {
+      this.$ionicScrollDelegate.$getByHandle('chatScroll').scrollBottom(animate);
+    }, 300);
   }
 }
