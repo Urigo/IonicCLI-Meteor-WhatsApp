5e0f7b5bfb35507421fbc8485ab3cc9d3eb56f92
DAB0mB <emanor6@gmail.com>
2016-04-25 22:17:59 +0300

    Step 4.17: Add input directive

---
+++ src/directives/input.directive.js
@@ -0,0 +1,47 @@
+import { Directive } from 'angular-ecmascript/module-helpers';
+
+export default class InputDirective extends Directive {
+  static $inject = ['$timeout']
+  static $name = 'input'
+
+  restrict = 'E'
+
+  scope = {
+    'returnClose': '=',
+    'onReturn': '&',
+    'onFocus': '&',
+    'onBlur': '&'
+  }
+
+  link(scope, element) {
+    element.bind('focus', (e) => {
+      if (!scope.onFocus) return;
+
+      this.$timeout(() => {
+        scope.onFocus();
+      });
+    });
+
+    element.bind('blur', (e) => {
+      if (!scope.onBlur) return;
+
+      this.$timeout(() => {
+        scope.onBlur();
+      });
+    });
+
+    element.bind('keydown', (e) => {
+      if (e.which != 13) return;
+
+      if (scope.returnClose) {
+        element[0].blur();
+      }
+
+      if (scope.onReturn) {
+        this.$timeout(() => {
+          scope.onReturn();
+        });
+      }
+    });
+  }
+}
