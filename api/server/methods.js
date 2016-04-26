4a6584a878bfcd274cd2f906dd689accc775fec7
DAB0mB <emanor6@gmail.com>
2016-04-26 19:26:02 +0300

    Step 4.26: Add validation to new message method

--- api/server/methods.js
+++ api/server/methods.js
@@ -1,8 +1,14 @@
 import { Meteor } from 'meteor/meteor';
+import { check as Check } from 'meteor/check';
 import { Cahts, Messages } from './collections';
 
 Meteor.methods({
   newMessage(message) {
+    Check(message, {
+      text: String,
+      chatId: String
+    });
+
     message.timestamp = new Date();
 
     const messageId = Messages.insert(message);
