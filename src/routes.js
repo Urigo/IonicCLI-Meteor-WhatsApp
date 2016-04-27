d2e4194b86cbb7d358f0982233f3b07c1eeb85e4
DAB0mB <emanor6@gmail.com>
2016-04-28 01:38:14 +0300

    Step 5.6: Add auth resolve to route states

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,21 @@ import { Config } from 'angular-ecmascript/module-helpers';
 export default class RoutesConfig extends Config {
   static $inject = ['$stateProvider', '$urlRouterProvider']
 
+  constructor() {
+    super(...arguments);
+
+    this.isAuthorized = ['$auth', this::this.isAuthorized];
+  }
+
   configure() {
     this.$stateProvider
       .state('tab', {
         url: '/tab',
         abstract: true,
-        templateUrl: 'templates/tabs.html'
+        templateUrl: 'templates/tabs.html',
+        resolve: {
+          user: this.isAuthorized
+        }
       })
       .state('tab.chats', {
         url: '/chats',
@@ -30,5 +39,9 @@ export default class RoutesConfig extends Config {
       });
 
     this.$urlRouterProvider.otherwise('tab/chats');
+  }
+
+  isAuthorized($auth) {
+    return $auth.awaitUser();
   }
 }
