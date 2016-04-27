e401ee9acb4feccd2f21862e8d4f6999682f626e
DAB0mB <emanor6@gmail.com>
2016-04-28 01:49:05 +0300

    Step 5.20: Add profile controller

---
+++ src/controllers/profile.controller.js
@@ -0,0 +1,34 @@
+import { _ } from 'meteor/underscore';
+import { Controller } from 'angular-ecmascript/module-helpers';
+
+export default class ProfileCtrl extends Controller {
+  static $inject = ['$state', '$ionicPopup', '$log']
+
+  constructor() {
+    super(...arguments);
+
+    const profile = this.currentUser && this.currentUser.profile;
+    this.name = profile ? profile.name : '';
+  }
+
+  updateName() {
+    if (_.isEmpty(this.name)) return;
+
+    this.callMethod('updateName', this.name, (err) => {
+      if (err) return this.handleError(err);
+      this.$state.go('tab.chats');
+    });
+  }
+
+  handleError(err) {
+    this.$log.error('Profile save error ', err);
+
+    this.$ionicPopup.alert({
+      title: err.reason || 'Save failed',
+      template: 'Please try again',
+      okType: 'button-positive button-clear'
+    });
+  }
+}
+
+ProfileCtrl.$name = 'ProfileCtrl';
