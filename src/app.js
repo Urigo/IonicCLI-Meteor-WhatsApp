df14bcaccd4bbb9cc4695e23612b682fd1e64505
DAB0mB <emanor6@gmail.com>
2016-04-28 13:02:29 +0300

    Step 6.7: Load new chat controller

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import ChatCtrl from './controllers/chat.controller';
 import ChatsCtrl from './controllers/chats.controller';
 import ConfirmationCtrl from './controllers/confirmation.controller';
 import LoginCtrl from './controllers/login.controller';
+import NewChatCtrl from './controllers/new-chat.controller';
 import ProfileCtrl from './controllers/profile.controller';
 import SettingsCtrl from './controllers/settings.controller';
 import InputDirective from './directives/input.directive';
@@ -27,6 +28,7 @@ new Loader(App)
   .load(ChatsCtrl)
   .load(ConfirmationCtrl)
   .load(LoginCtrl)
+  .load(NewChatCtrl)
   .load(ProfileCtrl)
   .load(SettingsCtrl)
   .load(InputDirective)
