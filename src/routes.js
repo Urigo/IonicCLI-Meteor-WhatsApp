f42207781d94448d3ca815da6630bcc44ff75bef
DAB0mB <emanor6@gmail.com>
2016-05-01 23:00:08 +0300

    Step 5.18: Add confirmation route state

--- src/routes.js
+++ src/routes.js
@@ -41,6 +41,11 @@ export class RoutesConfig extends Config {
         url: '/login',
         templateUrl: 'templates/login.html',
         controller: 'LoginCtrl as logger'
+      })
+      .state('confirmation', {
+        url: '/confirmation/:phone',
+        templateUrl: 'templates/confirmation.html',
+        controller: 'ConfirmationCtrl as confirmation'
       });
 
     this.$urlRouterProvider.otherwise('tab/chats');
